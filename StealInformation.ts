82ff44d1726957cf49a3aed7c9b6866a769139b4
ruriko123 <hriayesh.lekhak@gmail.com>
2023-10-02 18:37:14 -0500

    refactored the code

--- StealInformation.ts
+++ StealInformation.ts
@@ -1,30 +1,33 @@
-import pageJson from './page.json'
-import {StringStream, readableToString} from '@rauschma/stringio';
-const puppeteer = require('puppeteer-extra');
-const hidden = require('puppeteer-extra-plugin-stealth')
-const chromium = require('chromium');
+const pageJson = require('./page.json');
+const puppeteer = require('puppeteer');
 const fs = require("fs");
 
-async function getStocks(cName) {
+
+
+
+
+export async function getStocks(cName: string) {
 
     try {
 
         // Launch sequence
-        puppeteer.use(hidden())
         const browser = await puppeteer.launch({
             args: [
-                '--single-process', '--no-zygote', '--no-sandbox'
+                "--disable-gpu",
+                "--disable-dev-shm-usage",
+                "--no-sandbox",
+                "--disable-setuid-sandbox",
+                "--no-first-run",
+                "--no-zygote",
+                "--single-process",
             ],
             headless: false,
             ignoreHTTPSErrors: true,
-
-            // add this
-            executablePath: chromium.path
         })
 
         const page = await browser.newPage()
         await page.setViewport({width: 1920, height: 1280, deviceScaleFactor: 1});
-        await page.goto(`${pageJson.name}${cName}`, {waitUntil: 'networkidle0'});
+        await page.goto(`${pageJson.name}${cName}`, {waitUntil: "domcontentloaded" , timeout: 60000});
         await page.click('#ctl00_ContentPlaceHolder1_CompanyDetail1_lnkHistoryTab');
 
         await page
@@ -39,11 +42,9 @@ async function getStocks(cName) {
 
         let element = await page.$('#ctl00_ContentPlaceHolder1_CompanyDetail1_PagerControlTransactionHistory1_litRec' +
                 'ords')
-        let maxPage = await page.evaluate(el => el.textContent, element)
+        let maxPage:any = await page.evaluate((el: { textContent: any; }) => el?.textContent, element)
 
-        let Maxpage = maxPage
-            .match(/Total pages: \d+/gm)
-            .toString()
+        let Maxpage = maxPage?.match(/Total pages: \d+/gm)?.toString()
         Maxpage = parseInt(Maxpage.toString().match(/\d+/gm))
 
         for (let index = 1; index <= Maxpage; index++) {
@@ -54,8 +55,7 @@ async function getStocks(cName) {
                     .waitForSelector(`a[title="Page ${index + 1}"]`, {visible: true})
                     .then(() => console.log('Next page loaded', index));
 
-           
-
+                }
             let PageNumberSelector = `a[title="Page ${index}"]`
 
             await page.click(PageNumberSelector);
@@ -66,19 +66,19 @@ async function getStocks(cName) {
 
             const result = await page.evaluate(() => {
 
-                const rows = Array.from(document.querySelectorAll('#ctl00_ContentPlaceHolder1_CompanyDetail1_divDataPrice > div.table-responsive > ' +
+                const rows:any = Array.from(document.querySelectorAll('#ctl00_ContentPlaceHolder1_CompanyDetail1_divDataPrice > div.table-responsive > ' +
                         'table > tbody'));
-                return rows.map(td => td.innerText);
+                return rows.map((td: { innerText: any; }) => td?.innerText);
             });
 
             let csvresult = await result[0].replace(/;/g, ",");
 
             !fs.existsSync(`./stockDetails/${cName}`) && fs.mkdirSync(`./stockDetails/${cName}`, {recursive: true})
 
-            fs.writeFile(`./stockDetails/${cName}/data${index}.csv`, csvresult, (err) => {
+            fs.writeFile(`./stockDetails/${cName}/data${index}.csv`, csvresult, (err: any) => {
                 console.log(err || "done");
             });
-        }
+    
             if (index < Maxpage) {
                 await page.click(`a[title="Page ${index + 1}"]`);
             }
@@ -93,4 +93,6 @@ async function getStocks(cName) {
     }
 
 }
-getStocks("EBL")
+
+
+
